31d38f36707dc0e2740de3f4da49eeb0e6f98cec
Vineet Gupta <vineetgupta22@gmail.com>
2013-12-08 14:06:21 +0530

    Starting the Project for Demo
    Signed-off-by: Vineet Gupta <vineetgupta22@gmail.com>

---
+++ index.php
@@ -0,0 +1,20 @@
+<?
+
+	/**
+	*	Starting of Project for Demo
+	*
+	*	This file would be used to demonstrate the project, usage and other
+	*	things. For starting Demonstration run on the server.
+	*
+	*	@package				gContacts
+	*	@file					index.php
+	*	@copyright				Cee Emm Infotech, 2013-2014
+	*	@author					Vineet Gupta <vineetgupta22@gmail.com>
+	*	@since					1.0.0
+	*	
+	**/
+
+	
+	require_once 'gContacts.php';
+
+?>
