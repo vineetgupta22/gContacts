02c98b77bc530ab15ae6eb8e7a7a54b65c0bac64
Vineet Gupta <vineetgupta22@gmail.com>
2013-12-08 22:00:10 +0530

    Correction of Version file location

--- lib/version/version.php
+++ lib/version/version.php
@@ -55,7 +55,7 @@
 		*	gContacts Changes Release Version for tracking of Errors and Reporting
 		*	Purposes
 		*/
-		var $gContacts_Changes_Release = 11;
+		var $gContacts_Changes_Release = 12;
 		
 		
 	}
