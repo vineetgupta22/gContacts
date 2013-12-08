def7ec8b6b33c680cf885d59782489f39ad32e6a
Vineet Gupta <vineetgupta22@gmail.com>
2013-12-08 18:02:34 +0530

    Added the referance to framework
    Added the defination of root directory and then including the framework
    file to the project. It makes the things little simple to do.
    
    
    Signed-off-by: Vineet Gupta <vineetgupta22@gmail.com>

--- gContacts.php
+++ gContacts.php
@@ -15,13 +15,31 @@
 	*	
 	**/
 
+
+	// {{{ constants
+
 	/**
 	*	Preprocessor to be included in every file for checking how the
 	*	user is accessing the file it is required to be defined and if
 	*	not defined then code/class/function not included but die at the 
 	*	instance. To make impossible to access page without controller. 
 	*	An implementation and working for MVC.
-	*/
+	**/
 	define('gContacts', true);
 
+	/**
+	*	Current Location of the Directory required to include other files
+	*	and library to the Project Dynamically. The location of file is 
+	*	important as to Dynamically include Library and Functions files
+	**/
+	define('_root', realpath(dirname(__FILE__)));
+
+	// }}}
+
+
+	/**
+	*	Including the Framework file to the Project, as it will further load
+	*	more important Library files and create a minimum working environment.
+	**/
+	require_once ( _root . "/lib/framework/framework.php" );
 ?>
