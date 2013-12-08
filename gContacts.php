f944672a5920c53975c7833b20f444764beba47a
Vineet Gupta <vineetgupta22@gmail.com>
2013-12-08 14:06:40 +0530

    Single File Skelton for including library
    Signed-off-by: Vineet Gupta <vineetgupta22@gmail.com>

---
+++ gContacts.php
@@ -0,0 +1,27 @@
+<?
+
+	/**
+	*	gContacts - Single File Inclusion 
+	*
+	*	This file would be used to include the Library, Functions
+	*	Objects working and other things. This file is used by
+	*	both Projects i.e. Project for Demo and Project Developers.
+	*
+	*	@package				gContacts
+	*	@file					gContacts.php
+	*	@copyright				Cee Emm Infotech, 2013-2014
+	*	@author					Vineet Gupta <vineetgupta22@gmail.com>
+	*	@since					1.0.0
+	*	
+	**/
+
+	/**
+	*	Preprocessor to be included in every file for checking how the
+	*	user is accessing the file it is required to be defined and if
+	*	not defined then code/class/function not included but die at the 
+	*	instance. To make impossible to access page without controller. 
+	*	An implementation and working for MVC.
+	*/
+	define('gContacts', true);
+
+?>
